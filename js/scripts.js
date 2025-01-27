bea936b1a922daa5d5014ca0ad18f7accd4e5d76
User <crosas-2023500@kinal.edu.gt>
2025-01-26 21:39:33 -0600

    Tarea Finalizada

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,6 @@ window.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('scroll', function() {
         const currentTop = document.body.getBoundingClientRect().top * -1;
         if ( currentTop < scrollPos) {
-            // Scrolling Up
             if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
                 mainNav.classList.add('is-visible');
             } else {
@@ -13,7 +12,6 @@ window.addEventListener('DOMContentLoaded', () => {
                 mainNav.classList.remove('is-visible', 'is-fixed');
             }
         } else {
-            // Scrolling Down
             mainNav.classList.remove(['is-visible']);
             if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
                 mainNav.classList.add('is-fixed');
